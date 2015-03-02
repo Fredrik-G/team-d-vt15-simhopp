51ec623027292e812036905b384d89b63af32c5b
gregorius102@gmail.com <gregorius102@gmail.com@13972cca-7f3e-42ca-aa13-33eae2422b70>
2015-03-02 14:34:29 +0000

    Class that holds "help classes" when communicating via TCP-connection

---
+++ trunk/Simhopp/HelpClassesForTcp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simhopp
+{
+    public class ServerObjectData
+    {
+        public string ContestName { get; set; }
+        public string DiverName { get; set; }
+        public string TrickName { get; set; }
+        public double TrickDiff { get; set; }
+        public ServerObjectData() { }
+        public ServerObjectData(string cName, string dName, string tName, double tDiff)
+        {
+            this.ContestName = cName;
+            this.DiverName = dName;
+            this.TrickName = tName;
+            this.TrickDiff = tDiff;
+        }
+    };
+    public class ClientObjectData
+    {
+        public string JudgeName { get; set; }
+        public double Point { get; set; }
+
+        public ClientObjectData() { }
+
+        public ClientObjectData(string JudgeName, double Point)
+        {
+            this.JudgeName = JudgeName;
+            this.Point = Point;
+        }
+
+    };
+}
